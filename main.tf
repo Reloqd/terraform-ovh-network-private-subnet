27f06665c90fcaa63bfb8f97e9ca3c48191c2e49
Steven maulny <steven.maulny@gmail.com>
2025-12-05 23:41:20 +0100

    Add module ovh subnet for one or multiple region OVH

--- main.tf
+++ main.tf
@@ -1,40 +1,33 @@
-
 locals {
-  network_private_subnet_list = [
+    default_subnets = [
     {
-      network_private_subnet_region     = "GRA9"
-      network_private_subnet_cidr       = "10.0.1.0/24"
-      network_private_subnet_start_ip   = "10.0.1.10"
-      network_private_subnet_end_ip     = "10.0.1.200"
-      network_private_subnet_dhcp       = true
-      network_private_subnet_no_gateway = false
-    },
-    {
-      network_private_subnet_region     = "SBG5"
-      network_private_subnet_cidr       = "10.0.2.0/24"
-      network_private_subnet_start_ip   = "10.0.2.10"
-      network_private_subnet_end_ip     = "10.0.2.200"
-      network_private_subnet_dhcp       = true
-      network_private_subnet_no_gateway = false
-    }    
+      network_private_subnet_network_region = "GRA9"
+      network_private_subnet_cidr           = "10.0.1.0/24"
+      network_private_subnet_start_ip       = "10.0.1.10"
+      network_private_subnet_end_ip         = "10.0.1.200"
+      network_private_subnet_dhcp_enabled   = true
+      network_private_subnet_no_gateway     = false
+    }
   ]
+
+  effective_subnets = (
+    length(var.network_private_subnets) > 0 ? var.network_private_subnets : local.default_subnets
+  )
 }
+
 resource "ovh_cloud_project_network_private_subnet" "network_private_subnet" {
-    for_each = length(var.network_private_subnets) == 0 ? {
-    for i, subnet in locals.network_private_subnet_list :
-    i => subnet
-  } : {
-    for i, subnet in var.network_private_subnets :
+  for_each = {
+    for i, subnet in local.effective_subnets :
     i => subnet
   }
-  
+
   service_name = var.network_private_subnet_service_name
   network_id   = var.network_private_subnet_network_id
 
-  region       = each.value.network_private_subnet_network_region
-  start        = each.value.network_private_subnet_start_ip
-  end          = each.value.network_private_subnet_end_ip
-  network      = each.value.network_private_subnet_cidr
-  dhcp         = each.value.network_private_subnet_dhcp_enabled
-  no_gateway   = each.value.network_private_subnet_no_gateway
+  region     = each.value.network_private_subnet_network_region
+  network    = each.value.network_private_subnet_cidr
+  start      = each.value.network_private_subnet_start_ip
+  end        = each.value.network_private_subnet_end_ip
+  dhcp       = each.value.network_private_subnet_dhcp_enabled
+  no_gateway = each.value.network_private_subnet_no_gateway
 }
